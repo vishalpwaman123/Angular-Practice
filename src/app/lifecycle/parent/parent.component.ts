3dfe4985eb182acd01f69781360abc01fbcf6c35
vishalpwaman <vishalpwaman1997@gmail.com>
2024-11-02 14:47:58 +0530

    Added : Angular pipe

--- src/app/lifecycle/parent/parent.component.ts
+++ src/app/lifecycle/parent/parent.component.ts
@@ -6,13 +6,20 @@ import { Component, SimpleChanges } from '@angular/core';
   styleUrl: './parent.component.css',
 })
 export class ParentComponent {
-  name: string = '';
+  _name: string = '';
+  _date: Date = new Date();
+  user = {
+    name: 'John Doe',
+    age: 30,
+    location: 'New York'
+  };
   constructor() {
     console.log('Parent Constructor Calling.....');
   }
 
   handleSubmit(event: HTMLInputElement) {
     console.log(event.value);
+    this._name = event.value;
     console.log(event.type);
     console.log(event.name);
     console.log(event.placeholder);
